850a8b553601c29e54ac022e535eecab3abb39d5
Arkadiusz <stefanski.a00@gmail.com>
2024-02-02 07:53:08 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    "Drink": {
+        "Wine": {},
+        "Schnaps": {}
+    },
+    "Fruit": {
+        "Red": {
+            "Cherry": {},
+            "Strawberry": {}
+        },
+        "Yellow": {
+            "Banana": {},
+            "Pineapple": {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const list = document.createElement("ul");
+    for(const keys in data){
+        const listItems = document.createElement("li");
+        listItems.innerText = keys;
+        list.append(listItems);
+        element.append(list);
+        if (data[keys]) createTree(listItems, data[keys]);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
